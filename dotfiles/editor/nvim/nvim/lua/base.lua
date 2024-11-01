c2a0ba002beb7d1666e7931a3dc09fe46bebc738
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2024-11-01 19:03:17 +0900

    update: nvim plugin

---
+++ dotfiles/editor/nvim/nvim/lua/base.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader  = ' '
+
+vim.o.number = true
+vim.wo.relativenumber = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.hlsearch = true
+vim.o.mouse = ''
+vim.o.backup = false
+vim.o.backupcopy = 'yes'
+vim.o.clipboard = 'unnamedplus'
+
+-- pane config
+vim.api.nvim_set_keymap(
+  'n', '<leader>-', '<C-w>s', { noremap = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '<leader>\\', '<C-w>v', { noremap = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '<leader>w', '<C-w>w', { noremap = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '<leader>r', '<C-w><', { noremap = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '<leader>l', '<C-w>>', { noremap = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '<leader>u', '<C-w>+', { noremap = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '<leader>b', '<C-w>-', { noremap = true }
+)
