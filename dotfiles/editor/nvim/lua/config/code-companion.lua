d0ead24b4a2f4bb7c7cb328c6db7614c8d9faf37
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2025-05-22 22:30:19 +0900

    add: ai plugin

---
+++ dotfiles/editor/nvim/lua/config/code-companion.lua
@@ -0,0 +1,104 @@
+require("codecompanion").setup({
+  display = {
+    chat = {
+      show_header_separator = true,
+      window = {
+        layout = 'vertical',
+        position = 'right'
+      }
+    }
+  },
+  strategies = {
+    chat = {
+      roles = {
+        llm = function(adapter)
+          return " CodeCompanion (" .. adapter.formatted_name .. ")"
+        end,
+        user = " Me",
+      }
+    },
+  },
+  adapters = {
+  },
+  opts = {
+    log_level = 'DEBUG',
+    language = 'Japanese'
+  }
+})
+
+vim.keymap.set('n', '<leader>ca', '<cmd>CodeCompanionActions<CR>')
+vim.keymap.set('v', '<leader>ca', '<cmd>CodeCompanionActions<CR>')
+vim.keymap.set('n', '<leader>cc', '<cmd>CodeCompanionChat<CR>')
+vim.keymap.set('v', '<leader>cc', '<cmd>CodeCompanionChat<CR>')
+
+local progress = require("fidget.progress")
+
+local M = {}
+
+function M:init()
+  local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", {})
+
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "CodeCompanionRequestStarted",
+    group = group,
+    callback = function(request)
+      local handle = M:create_progress_handle(request)
+      M:store_progress_handle(request.data.id, handle)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "CodeCompanionRequestFinished",
+    group = group,
+    callback = function(request)
+      local handle = M:pop_progress_handle(request.data.id)
+      if handle then
+        M:report_exit_status(handle, request)
+        handle:finish()
+      end
+    end,
+  })
+end
+
+M.handles = {}
+
+function M:store_progress_handle(id, handle)
+  M.handles[id] = handle
+end
+
+function M:pop_progress_handle(id)
+  local handle = M.handles[id]
+  M.handles[id] = nil
+  return handle
+end
+
+function M:create_progress_handle(request)
+  return progress.handle.create({
+    title = " Requesting assistance (" .. request.data.strategy .. ")",
+    message = "In progress...",
+    lsp_client = {
+      name = M:llm_role_title(request.data.adapter),
+    },
+  })
+end
+
+function M:llm_role_title(adapter)
+  local parts = {}
+  table.insert(parts, adapter.formatted_name)
+  if adapter.model and adapter.model ~= "" then
+    table.insert(parts, "(" .. adapter.model .. ")")
+  end
+  return table.concat(parts, " ")
+end
+
+function M:report_exit_status(handle, request)
+  if request.data.status == "success" then
+    handle.message = "Completed"
+  elseif request.data.status == "error" then
+    handle.message = " Error"
+  else
+    handle.message = "󰜺 Cancelled"
+  end
+end
+
+return M
---
+++ dotfiles/editor/nvim/lua/plugins/ai.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "j-hui/fidget.nvim"
+    },
+    config = function()
+      require('config.code-companion'):init()
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.icons",
+    },
+    ft = {
+      "markdown",
+      "markdown.mdx",
+      "Avante",
+      "codecompanion"
+    },
+    opts = {
+      file_types = {
+        "markdown",
+        "Avante",
+        "codecompanion"
+      },
+    }
+  }
+}
